3ee076116f01a297b05074a6d713d84c3cad06ec
shane preston <spreston2009@gmail.com>
2019-02-05 15:13:42 +0000

    Update external.js

--- External JS Files/external.js
+++ External JS Files/external.js
@@ -1,1 +1,11 @@
-javascript
+class Hello extends React.Component {
+  render() {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <p>i'm from an external javascript file</p>
+        </div>);
+  }
+}
+
+ReactDOM.render(<Hello />, document.getElementById('intro'));
